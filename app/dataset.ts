720ab8ba7ba32cd970bdbb3b65870d3e63c2f57d
Viktor <vmac@itu.dk>
2022-09-11 18:09:07 +0200

    Adding initial files and dateset of users

---
+++ app/dataset.ts
@@ -0,0 +1,6 @@
+let users: { id: number; email: string, username: string}[] = [
+    { id: 1, email: "viki.macek@gmail.com", username: "charouzic"},
+    { id: 2, email: "jan.kriz@mail.com", username: "prograMind" },
+    { id: 3, email: "v.tetour@gmail.com", username: "vlastik" },
+  ];
+  export default users;
